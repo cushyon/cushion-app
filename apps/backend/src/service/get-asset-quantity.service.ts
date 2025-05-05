31c3bb3950d284d0698b12233658d05ca6c65f2f
chixx <lucottejeremie@gmail.com>
2025-05-05 20:25:14 +0200

    backend update getAssetQuantity to dont throw an error if token accoun data undefined

--- apps/backend/src/service/get-asset-quantity.service.ts
+++ apps/backend/src/service/get-asset-quantity.service.ts
@@ -6,7 +6,11 @@ export const getAssetQuantity = async (asset1: string, asset2: string) => {
   const tokenAccountBalance1 = await getTokenAccountData(asset1, PUBLIC_KEY);
   const tokenAccountBalance2 = await getTokenAccountData(asset2, PUBLIC_KEY);
   return {
-    asset1: tokenAccountBalance1.result.value,
-    asset2: tokenAccountBalance2.result.value,
+    asset1: tokenAccountBalance1?.result?.value
+      ? tokenAccountBalance1.result.value
+      : undefined,
+    asset2: tokenAccountBalance2?.result?.value
+      ? tokenAccountBalance2.result.value
+      : undefined,
   };
 };
