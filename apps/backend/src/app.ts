2e570f7bc120ceba45aa385414f94f5cf5bd4735
chixx <lucottejeremie@gmail.com>
2025-05-05 14:56:05 +0200

    backend add price and quantity api route

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import "./cron/rebalance";
 import { rebalance } from "./utils/rebalance";
 import { getAssetPairPrice } from "./service/get-asset-pair-price.service";
+import { getAssetQuantity } from "./service/get-asset-quantity.service";
 
 const app: Express = express();
 
@@ -13,6 +14,19 @@ app.get("/api/get-asset-pair-price", async (req: Request, res: Response) => {
   const { asset1, asset2 } = req.body;
   const price = await getAssetPairPrice(asset1, asset2);
   res.json({ price });
+});
+
+app.get("/api/get-asset-quantity", async (req: Request, res: Response) => {
+  const { asset1, asset2 } = req.body;
+  const quantity = await getAssetQuantity(asset1, asset2);
+  res.json({ quantity });
+});
+
+app.get("/price-and-quantity", async (req: Request, res: Response) => {
+  const { asset1, asset2 } = req.body;
+  const price = await getAssetPairPrice(asset1, asset2);
+  const quantity = await getAssetQuantity(asset1, asset2);
+  res.json({ price, quantity });
 });
 
 app.post("/api/rebalance", async (req: Request, res: Response) => {
