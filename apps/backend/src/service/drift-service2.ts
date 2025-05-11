7ad9fa60a5aca17ff23643139fa468c7800cfc92
chixx <lucottejeremie@gmail.com>
2025-05-11 12:49:08 +0200

    push get current price without comment

--- apps/backend/src/service/drift-service2.ts
+++ apps/backend/src/service/drift-service2.ts
@@ -1,128 +1,135 @@
-import * as anchor from '@coral-xyz/anchor';
-import { getAssociatedTokenAddress } from '@solana/spl-token';
-import bs58 from 'bs58';
+import * as anchor from "@coral-xyz/anchor";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import bs58 from "bs58";
 
-import { Keypair, PublicKey, type TransactionInstruction, ComputeBudgetProgram} from '@solana/web3.js';
 import {
-	DriftClient,
-	PositionDirection,
-	OrderType,
-	User,
-	getUserAccountPublicKeySync,
-	initialize,
-	BulkAccountLoader,
-	DRIFT_PROGRAM_ID,
-	getOrderParams,
-	getLimitOrderParams,
-	numberToSafeBN,
-	MarketType,
-	PostOnlyParams,
-	PRICE_PRECISION,
-	BASE_PRECISION
-} from '@drift-labs/sdk';
-import { Public } from '@prisma/client/runtime/library';
+  Keypair,
+  PublicKey,
+  type TransactionInstruction,
+  ComputeBudgetProgram,
+} from "@solana/web3.js";
+import {
+  DriftClient,
+  PositionDirection,
+  OrderType,
+  User,
+  getUserAccountPublicKeySync,
+  initialize,
+  BulkAccountLoader,
+  DRIFT_PROGRAM_ID,
+  getOrderParams,
+  getLimitOrderParams,
+  numberToSafeBN,
+  MarketType,
+  PostOnlyParams,
+  PRICE_PRECISION,
+  BASE_PRECISION,
+} from "@drift-labs/sdk";
+import { Public } from "@prisma/client/runtime/library";
 
 export const getTokenAddress = (
-	mintAddress: string,
-	userPubKey: string
+  mintAddress: string,
+  userPubKey: string
 ): Promise<PublicKey> => {
-	return getAssociatedTokenAddress(
-		new PublicKey(mintAddress),
-		new PublicKey(userPubKey)
-	);
+  return getAssociatedTokenAddress(
+    new PublicKey(mintAddress),
+    new PublicKey(userPubKey)
+  );
 };
 
 const main = async () => {
-	const env = 'devnet';
-	const sdkConfig = initialize({ env });
+  const env = "devnet";
+  const sdkConfig = initialize({ env });
 
-	const keypairBytes = bs58.decode(process.env.WALLET_PRIVATE_KEY || "");
-	const keypair = Keypair.fromSecretKey(keypairBytes);
+  const keypairBytes = bs58.decode(process.env.WALLET_PRIVATE_KEY || "");
+  const keypair = Keypair.fromSecretKey(keypairBytes);
 
-	// Set up the Wallet and Provider
-	if (!process.env.ANCHOR_WALLET) {
-		throw new Error('ANCHOR_WALLET env var must be set.');
-	}
+  // Set up the Wallet and Provider
+  if (!process.env.ANCHOR_WALLET) {
+    throw new Error("ANCHOR_WALLET env var must be set.");
+  }
 
-	if (!process.env.ANCHOR_PROVIDER_URL) {
-		throw new Error('ANCHOR_PROVIDER_URL env var must be set.');
-	}
-		
-	const provider = anchor.AnchorProvider.local(
-		process.env.ANCHOR_PROVIDER_URL,
-		{
-			preflightCommitment: 'confirmed',
-			skipPreflight: false,
-			commitment: 'confirmed',
-		}
-	);
-	
-	// Check SOL Balance
-	const lamportsBalance = await provider.connection.getBalance(
-		provider.wallet.publicKey
-	);
-	console.log(
-		provider.wallet.publicKey.toString(),
-		env,
-		'SOL balance:',
-		lamportsBalance / 10 ** 9
-	);
+  if (!process.env.ANCHOR_PROVIDER_URL) {
+    throw new Error("ANCHOR_PROVIDER_URL env var must be set.");
+  }
 
-	const authorityaddy = new PublicKey("6hpk9equdGMJf1pKs9xcuwUrMigCYC5Gec15tCbMqcs8");
+  const provider = anchor.AnchorProvider.local(
+    process.env.ANCHOR_PROVIDER_URL,
+    {
+      preflightCommitment: "confirmed",
+      skipPreflight: false,
+      commitment: "confirmed",
+    }
+  );
 
-	const driftClientVault = new DriftClient({
-			connection: provider.connection,
-			wallet: provider.wallet,
-			env: "devnet", 
-			accountSubscription: {
-				type: 'websocket',
-			},
-			authority: authorityaddy,
-			subAccountIds: [0],
-			activeSubAccountId: 0
-		});
-	
-	await driftClientVault.subscribe();
+  // Check SOL Balance
+  const lamportsBalance = await provider.connection.getBalance(
+    provider.wallet.publicKey
+  );
+  console.log(
+    provider.wallet.publicKey.toString(),
+    env,
+    "SOL balance:",
+    lamportsBalance / 10 ** 9
+  );
 
-		// Get current price
-		const solMarketInfo = PerpMarkets[env].find(
-			(market) => market.baseAssetSymbol === 'SOL'
-		);
-	
-		//const marketIndex = solMarketInfo.marketIndex;
-	
-		// Get vAMM bid and ask price
-		// const [bid, ask] = calculateBidAskPrice(
-		// 	driftClient.getPerpMarketAccount(marketIndex).amm,
-		// 	driftClient.getOracleDataForPerpMarket(marketIndex)
-		// );
-	
-		// const formattedBidPrice = convertToNumber(bid, PRICE_PRECISION);
-		// const formattedAskPrice = convertToNumber(ask, PRICE_PRECISION);
-	
-		// console.log(
-		// 	env,
-		// 	`vAMM bid: $${formattedBidPrice} and ask: $${formattedAskPrice}`
-		// );
-	
-		// const solMarketAccount = driftClient.getPerpMarketAccount(
-		// 	solMarketInfo.marketIndex
-		// );
-		// console.log(env, `Placing a 1 SOL-PERP LONG order`);
-	
-		// const txSig = await driftClient.placePerpOrder(
-		// 	getMarketOrderParams({
-		// 		baseAssetAmount: new BN(1).mul(BASE_PRECISION),
-		// 		direction: PositionDirection.LONG,
-		// 		marketIndex: solMarketAccount.marketIndex,
-		// 	})
-		// );
-		// console.log(
-		// 	env,
-		// 	`Placed a 1 SOL-PERP LONG order. Tranaction signature: ${txSig}`
-		// );
+  const authorityaddy = new PublicKey(
+    "6hpk9equdGMJf1pKs9xcuwUrMigCYC5Gec15tCbMqcs8"
+  );
 
-	/*const orderParams = {
+  const driftClientVault = new DriftClient({
+    connection: provider.connection,
+    wallet: provider.wallet,
+    env: "devnet",
+    accountSubscription: {
+      type: "websocket",
+    },
+    authority: authorityaddy,
+    subAccountIds: [0],
+    activeSubAccountId: 0,
+  });
+
+  await driftClientVault.subscribe();
+
+  // Get current price
+  const solMarketInfo = PerpMarkets[env].find(
+    (market) => market.baseAssetSymbol === "SOL"
+  );
+
+  const marketIndex = solMarketInfo.marketIndex;
+
+  // Get vAMM bid and ask price
+  const [bid, ask] = calculateBidAskPrice(
+    driftClient.getPerpMarketAccount(marketIndex).amm,
+    driftClient.getOracleDataForPerpMarket(marketIndex)
+  );
+
+  const formattedBidPrice = convertToNumber(bid, PRICE_PRECISION);
+  const formattedAskPrice = convertToNumber(ask, PRICE_PRECISION);
+
+  console.log(
+    env,
+    `vAMM bid: $${formattedBidPrice} and ask: $${formattedAskPrice}`
+  );
+
+  const solMarketAccount = driftClient.getPerpMarketAccount(
+    solMarketInfo.marketIndex
+  );
+  console.log(env, `Placing a 1 SOL-PERP LONG order`);
+
+  const txSig = await driftClient.placePerpOrder(
+    getMarketOrderParams({
+      baseAssetAmount: new BN(1).mul(BASE_PRECISION),
+      direction: PositionDirection.LONG,
+      marketIndex: solMarketAccount.marketIndex,
+    })
+  );
+  console.log(
+    env,
+    `Placed a 1 SOL-PERP LONG order. Tranaction signature: ${txSig}`
+  );
+
+  /*const orderParams = {
 		orderType: OrderType.LIMIT,
 		marketIndex: 1,
 		direction: PositionDirection.SHORT,
@@ -132,7 +139,7 @@ const main = async () => {
 	  
 	  await driftClientVault.placeSpotOrder(orderParams);*/
 
-	  /*const usdcSpotMarket = driftClientVault.getSpotMarketAccount(0);
+  /*const usdcSpotMarket = driftClientVault.getSpotMarketAccount(0);
     if (!usdcSpotMarket) {
       throw new Error("USDC-SPOT market not found");
     }
