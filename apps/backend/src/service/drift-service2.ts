046d3825f33c38de2766972d56f5b7894b1f5ed6
Odomart <odomart69@gmail.com>
2025-05-11 12:48:13 +0200

    test long not working

--- apps/backend/src/service/drift-service2.ts
+++ apps/backend/src/service/drift-service2.ts
@@ -2,7 +2,7 @@ import * as anchor from '@coral-xyz/anchor';
 import { getAssociatedTokenAddress } from '@solana/spl-token';
 import bs58 from 'bs58';
 
-import { Keypair, PublicKey } from '@solana/web3.js';
+import { Keypair, PublicKey, type TransactionInstruction, ComputeBudgetProgram} from '@solana/web3.js';
 import {
 	DriftClient,
 	PositionDirection,
@@ -10,7 +10,15 @@ import {
 	User,
 	getUserAccountPublicKeySync,
 	initialize,
-	BulkAccountLoader
+	BulkAccountLoader,
+	DRIFT_PROGRAM_ID,
+	getOrderParams,
+	getLimitOrderParams,
+	numberToSafeBN,
+	MarketType,
+	PostOnlyParams,
+	PRICE_PRECISION,
+	BASE_PRECISION
 } from '@drift-labs/sdk';
 import { Public } from '@prisma/client/runtime/library';
 
@@ -76,53 +84,45 @@ const main = async () => {
 	
 	await driftClientVault.subscribe();
 
-	/*const driftClient = new DriftClient({
-		connection: provider.connection,
-		wallet: provider.wallet,
-		env: 'devnet',
-	  });
-	  
-	  await driftClient.subscribe();*/
-
-	const bulkAccountLoader = new BulkAccountLoader(
-		provider.connection,
-		'confirmed',
-		1000
-	);
-	// Set up user client
-	const user = new User({
-		driftClient: driftClientVault,
-		userAccountPublicKey: getUserAccountPublicKeySync(
-			new PublicKey(authorityaddy),
-			new PublicKey("76X1E2EAdB2ZFB8gt4cXf8Qe9CJJH6dmxwYfnX5vv23m"),
-		),
-		accountSubscription: {
-			type: 'polling',
-			accountLoader: bulkAccountLoader,
-		},
-	});
-
-	const userAccountExists = await user.exists();
-    console.log('userAccountExists', userAccountExists);
-	if (!userAccountExists) {
-		console.log(
-			'initializing to',
-			env,
-			' drift account for',
-			provider.wallet.publicKey.toString()
+		// Get current price
+		const solMarketInfo = PerpMarkets[env].find(
+			(market) => market.baseAssetSymbol === 'SOL'
 		);
+	
+		//const marketIndex = solMarketInfo.marketIndex;
+	
+		// Get vAMM bid and ask price
+		// const [bid, ask] = calculateBidAskPrice(
+		// 	driftClient.getPerpMarketAccount(marketIndex).amm,
+		// 	driftClient.getOracleDataForPerpMarket(marketIndex)
+		// );
+	
+		// const formattedBidPrice = convertToNumber(bid, PRICE_PRECISION);
+		// const formattedAskPrice = convertToNumber(ask, PRICE_PRECISION);
+	
+		// console.log(
+		// 	env,
+		// 	`vAMM bid: $${formattedBidPrice} and ask: $${formattedAskPrice}`
+		// );
+	
+		// const solMarketAccount = driftClient.getPerpMarketAccount(
+		// 	solMarketInfo.marketIndex
+		// );
+		// console.log(env, `Placing a 1 SOL-PERP LONG order`);
+	
+		// const txSig = await driftClient.placePerpOrder(
+		// 	getMarketOrderParams({
+		// 		baseAssetAmount: new BN(1).mul(BASE_PRECISION),
+		// 		direction: PositionDirection.LONG,
+		// 		marketIndex: solMarketAccount.marketIndex,
+		// 	})
+		// );
+		// console.log(
+		// 	env,
+		// 	`Placed a 1 SOL-PERP LONG order. Tranaction signature: ${txSig}`
+		// );
 
-		//// Create a Drift V2 account
-		const [txSig, userPublickKey] = await driftClientVault.initializeUserAccount(
-			0,
-			"nadar cushion 2"
-		  );
-		console.log("drift account created", txSig);
-	}
-
-	await user.subscribe();
-
-	const orderParams = {
+	/*const orderParams = {
 		orderType: OrderType.LIMIT,
 		marketIndex: 1,
 		direction: PositionDirection.SHORT,
@@ -130,7 +130,54 @@ const main = async () => {
 		price: driftClientVault.convertToPricePrecision(100),
 	  }
 	  
-	  await driftClientVault.placeSpotOrder(orderParams);
+	  await driftClientVault.placeSpotOrder(orderParams);*/
+
+	  /*const usdcSpotMarket = driftClientVault.getSpotMarketAccount(0);
+    if (!usdcSpotMarket) {
+      throw new Error("USDC-SPOT market not found");
+    }
+
+    const perpMarketAccount = driftClientVault.getPerpMarketAccount(
+      1,
+    );
+
+    if (!perpMarketAccount) {
+      throw new Error(
+        "Invalid symbol: Drift doesn't have a market for this token",
+      );
+    }
+
+	  const instructions: TransactionInstruction[] = [];
+	  instructions.push(
+		ComputeBudgetProgram.setComputeUnitLimit({ units: 1400000 }),
+	  );
+      const instruction = await driftClientVault.getPlaceOrdersIx([
+        getOrderParams(
+          getLimitOrderParams({
+            price: numberToSafeBN(160, PRICE_PRECISION),
+            marketType: MarketType.PERP,
+            baseAssetAmount: numberToSafeBN(160, BASE_PRECISION),
+            direction: PositionDirection.SHORT,
+            marketIndex: perpMarketAccount.marketIndex,
+            postOnly: PostOnlyParams.SLIDE,
+          }),
+        ),
+      ]);
+
+      instructions.push(instruction);
+
+	  const driftLookupTableAccount = await driftClientVault.fetchMarketLookupTableAccount()
+
+	  const latestBlockhash = await driftClientVault.connection.getLatestBlockhash();
+		const tx = await driftClientVault.txSender.sendVersionedTransaction(
+		await driftClientVault.txSender.getVersionedTransaction(
+			instructions,
+			[driftLookupTableAccount],
+			[],
+			driftClientVault.opts,
+			latestBlockhash,
+		),
+		);*/
 };
 
 main();
