ffc58109633b5d546a314d79a9b509714cd889a5
nada-r <nadarzer@gmail.com>
2025-05-13 15:03:08 +0200

    set UI theme

--- apps/landing/app/layout.tsx
+++ apps/landing/app/layout.tsx
@@ -1,4 +1,15 @@
+import type React from "react";
 import "./global.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "../components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Cushion | Quantitative strategies for DeFi",
+  description:
+    "Cushion brings capital-protected strategies to DeFi, offering high-yield solutions with advanced risk management",
+};
 
 export default function RootLayout({
   children,
@@ -6,8 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
