7f2e4c27b71f978455102d0688e7bb5a2219a792
chixx <lucottejeremie@gmail.com>
2025-06-12 00:42:26 +0200

    backend add try catch for rebalance and logs

--- apps/backend/src/controller/rebalance.controller.ts
+++ apps/backend/src/controller/rebalance.controller.ts
@@ -9,9 +9,8 @@ import {
 } from "../service/google-sheets.service";
 import { normalisePercents } from "../utils/normalize-percents";
 
-
 export const rebalance = async (req: Request, res: Response) => {
-  console.log("Entering rebalance");
+  console.log("----- Entering rebalance -----");
   // Write the current date in the first column of the Google Sheet
   const currentDate = new Date().toISOString();
   const log: LogEntry = {
@@ -23,7 +22,12 @@ export const rebalance = async (req: Request, res: Response) => {
   try {
     // Get the data from the request body
     let { id, percentageAsset1, percentageAsset2 } = req.body;
-    console.log("percentageAsset1", percentageAsset1, "percentageAsset2", percentageAsset2);
+    console.log(
+      "percentageAsset1",
+      percentageAsset1,
+      "percentageAsset2",
+      percentageAsset2
+    );
     const log: LogEntry = {
       column: "B",
       value: percentageAsset1,
@@ -33,14 +37,19 @@ export const rebalance = async (req: Request, res: Response) => {
     log.value = percentageAsset2;
     await updateLastRow([log]);
     if (percentageAsset1 + percentageAsset2 !== 100) {
-      console.log("Percentage asset1 and asset2 must be 100, current value:", percentageAsset1 + percentageAsset2);
+      console.log(
+        "Percentage asset1 and asset2 must be 100, current value:",
+        percentageAsset1 + percentageAsset2
+      );
       //send a tg notif instead and round the values throw new Error("Percentage asset1 and asset2 must be 100");
       const { p1, p2 } = normalisePercents(percentageAsset1, percentageAsset2);
       percentageAsset1 = p1;
       percentageAsset2 = p2;
       console.log("Normalised percentages", percentageAsset1, percentageAsset2);
     }
-    return;
+    // return;
+
+    console.log("----- STARTING REBALANCE -----");
 
     const result = await rebalanceWithDrift(
       SOL.address,
@@ -48,6 +57,8 @@ export const rebalance = async (req: Request, res: Response) => {
       percentageAsset1,
       percentageAsset2
     );
+
+    console.log("----- REBALANCE COMPLETED -----");
 
     const logResult: LogEntry[] = [
       {
@@ -89,13 +100,15 @@ export const rebalance = async (req: Request, res: Response) => {
     ];
     await updateLastRow(logResult);
 
-    console.log("result", result);
+    console.log("Rebalance result", result);
     if (result.status === "success") {
+      console.log("----- Updating trade execution data -----");
       await updateTradeExecutionData(
         id,
         result?.nav ?? 0,
         req.body.initial_capital
       );
+      console.log("----- Trade execution data updated -----");
     }
     res.json({ message: "Rebalance done", result });
   } catch (error) {
--- apps/backend/src/service/rebalance-with-drift.service.ts
+++ apps/backend/src/service/rebalance-with-drift.service.ts
@@ -18,139 +18,160 @@ export const rebalanceWithDrift = async (
   percentageAsset1: number,
   percentageAsset2: number
 ) => {
-  const { driftClient, user } = await initDrift();
-  const { vaultDepositorAccount, depositorShares } = await initVaultDepositor({
-    driftClient,
-    vaultDepositorAddress: new PublicKey(
-      "4aXMMBox8pxMFABgVBg2MCxbFEcgm8cguFA5KRCFA6qf"
-    ),
-  });
+  try {
+    const { driftClient, user } = await initDrift();
+    const { vaultDepositorAccount, depositorShares } = await initVaultDepositor(
+      {
+        driftClient,
+        vaultDepositorAddress: new PublicKey(
+          "4aXMMBox8pxMFABgVBg2MCxbFEcgm8cguFA5KRCFA6qf"
+        ),
+      }
+    );
 
-  // console.log("vaultDepositorAccount", vaultDepositorAccount);
-  console.log("depositorShares", depositorShares);
+    // console.log("vaultDepositorAccount", vaultDepositorAccount);
+    console.log("depositorShares", depositorShares);
 
-  // Get the price of the asset1 and asset2
-  const asset1Data = getAssetData(asset1);
-  const asset2Data = getAssetData(asset2);
+    // Get the price of the asset1 and asset2
+    const asset1Data = getAssetData(asset1);
+    const asset2Data = getAssetData(asset2);
 
-  const oraclePriceAsset1 = driftClient.getOracleDataForSpotMarket(
-    asset1Data.marketIndex
-  );
-  const oraclePriceAsset2 = driftClient.getOracleDataForSpotMarket(
-    asset2Data.marketIndex
-  );
+    const oraclePriceAsset1 = driftClient.getOracleDataForSpotMarket(
+      asset1Data.marketIndex
+    );
+    const oraclePriceAsset2 = driftClient.getOracleDataForSpotMarket(
+      asset2Data.marketIndex
+    );
 
-  console.log("oraclePriceAsset1", oraclePriceAsset1);
-  console.log("oraclePriceAsset2", oraclePriceAsset2);
+    console.log("oraclePriceAsset1", oraclePriceAsset1);
+    console.log("oraclePriceAsset2", oraclePriceAsset2);
 
-  const formattedOraclePriceAsset1 = Number(oraclePriceAsset1.price) / 10 ** 6;
-  const formattedOraclePriceAsset2 = Number(oraclePriceAsset2.price) / 10 ** 6;
+    const formattedOraclePriceAsset1 =
+      Number(oraclePriceAsset1.price) / 10 ** 6;
+    const formattedOraclePriceAsset2 =
+      Number(oraclePriceAsset2.price) / 10 ** 6;
 
-  console.log("formattedOraclePriceAsset1", formattedOraclePriceAsset1);
-  console.log("formattedOraclePriceAsset2", formattedOraclePriceAsset2);
+    console.log("formattedOraclePriceAsset1", formattedOraclePriceAsset1);
+    console.log("formattedOraclePriceAsset2", formattedOraclePriceAsset2);
 
-  // Fetch portfolio Assets balance
-  const tokenAmountAsset1 = user.getTokenAmount(asset1Data.marketIndex);
-  const tokenAmountAsset2 = user.getTokenAmount(asset2Data.marketIndex);
+    // Fetch portfolio Assets balance
+    const tokenAmountAsset1 = user.getTokenAmount(asset1Data.marketIndex);
+    const tokenAmountAsset2 = user.getTokenAmount(asset2Data.marketIndex);
 
-  console.log("tokenAmountAsset1", tokenAmountAsset1);
-  console.log("tokenAmountAsset2", tokenAmountAsset2);
+    console.log("tokenAmountAsset1", tokenAmountAsset1);
+    console.log("tokenAmountAsset2", tokenAmountAsset2);
 
-  // Format the token amount in human readable format
-  const formattedTokenAmountAsset1 =
-    Number(tokenAmountAsset1) / 10 ** asset1Data.decimals;
-  const formattedTokenAmountAsset2 =
-    Number(tokenAmountAsset2) / 10 ** asset2Data.decimals;
+    // Format the token amount in human readable format
+    const formattedTokenAmountAsset1 =
+      Number(tokenAmountAsset1) / 10 ** asset1Data.decimals;
+    const formattedTokenAmountAsset2 =
+      Number(tokenAmountAsset2) / 10 ** asset2Data.decimals;
 
-  console.log("formattedTokenAmountAsset1", formattedTokenAmountAsset1);
-  console.log("formattedTokenAmountAsset2", formattedTokenAmountAsset2);
+    console.log("formattedTokenAmountAsset1", formattedTokenAmountAsset1);
+    console.log("formattedTokenAmountAsset2", formattedTokenAmountAsset2);
 
-  // Calculate the each asset value in USD
-  const amountAsset1InUSD =
-    formattedTokenAmountAsset1 * formattedOraclePriceAsset1;
-  const amountAsset2InUSD =
-    formattedTokenAmountAsset2 * formattedOraclePriceAsset2;
+    // Calculate the each asset value in USD
+    const amountAsset1InUSD =
+      formattedTokenAmountAsset1 * formattedOraclePriceAsset1;
+    const amountAsset2InUSD =
+      formattedTokenAmountAsset2 * formattedOraclePriceAsset2;
 
-  // Calculate the total NAV value in USD
-  const totalAmountInUSD = amountAsset1InUSD + amountAsset2InUSD;
+    // Calculate the total NAV value in USD
+    const totalAmountInUSD = amountAsset1InUSD + amountAsset2InUSD;
 
-  console.log("totalAmountInUSD", totalAmountInUSD);
-  console.log("amountAsset1InUSD", amountAsset1InUSD.toString());
-  console.log("amountAsset2InUSD", amountAsset2InUSD.toString());
+    console.log("totalAmountInUSD", totalAmountInUSD);
+    console.log("amountAsset1InUSD", amountAsset1InUSD.toString());
+    console.log("amountAsset2InUSD", amountAsset2InUSD.toString());
 
-  // Calculate the current percentage of each asset in the portfolio
-  const currentPercentageAsset1 = (amountAsset1InUSD / totalAmountInUSD) * 100;
-  const currentPercentageAsset2 = (amountAsset2InUSD / totalAmountInUSD) * 100;
+    // Calculate the current percentage of each asset in the portfolio
+    const currentPercentageAsset1 =
+      (amountAsset1InUSD / totalAmountInUSD) * 100;
+    const currentPercentageAsset2 =
+      (amountAsset2InUSD / totalAmountInUSD) * 100;
 
-  console.log("current percentageAsset1", currentPercentageAsset1);
-  console.log("current percentageAsset2", currentPercentageAsset2);
+    console.log("current percentageAsset1", currentPercentageAsset1);
+    console.log("current percentageAsset2", currentPercentageAsset2);
 
-  const tolerance = 1; // 1% tolerance, if variation above 1%, rebalance. Check and add control for min amount possible to swap
-  if (Math.abs(currentPercentageAsset1 - percentageAsset1) <= tolerance) {
-    console.log("No need to rebalance - within tolerance range");
+    const tolerance = 1; // 1% tolerance, if variation above 1%, rebalance. Check and add control for min amount possible to swap
+    if (Math.abs(currentPercentageAsset1 - percentageAsset1) <= tolerance) {
+      console.log("No need to rebalance - within tolerance range");
+      return {
+        //if no need to rebalance, return success
+        status: "success",
+        txSig: null,
+        currentPercentageAsset1,
+        currentPercentageAsset2,
+        swapDirection: null,
+        amountToSwap: 0,
+        amountToSwapInSOL: 0,
+        totalAmountInUSD,
+        nav: totalAmountInUSD * (depositorShares / 100),
+      };
+    }
+
+    const swapDirection =
+      currentPercentageAsset1 > percentageAsset1
+        ? "asset1ToAsset2"
+        : "asset2ToAsset1";
+
+    console.log("swapDirection", swapDirection);
+
+    const expectedAmountAsset1 = (percentageAsset1 / 100) * totalAmountInUSD;
+    const expectedAmountAsset2 = (percentageAsset2 / 100) * totalAmountInUSD;
+
+    console.log("expectedAmountAsset1", expectedAmountAsset1);
+    console.log("expectedAmountAsset2", expectedAmountAsset2);
+
+    if (swapDirection === "asset1ToAsset2") {
+      const swapResult = await swapSolForUsdc(
+        driftClient,
+        formattedTokenAmountAsset1,
+        formattedOraclePriceAsset1,
+        expectedAmountAsset1
+      );
+      return {
+        status: "success",
+        txSig: swapResult.txSig,
+        currentPercentageAsset1,
+        currentPercentageAsset2,
+        swapDirection,
+        amountToSwap: swapResult.amountToSwap,
+        amountToSwapInSOL: swapResult.amountToSwapInSOL,
+        totalAmountInUSD,
+        nav: totalAmountInUSD * (depositorShares / 100),
+      };
+    } else {
+      const swapResult = await swapUsdcForSol(
+        driftClient,
+        formattedOraclePriceAsset1,
+        formattedTokenAmountAsset2,
+        formattedOraclePriceAsset2,
+        expectedAmountAsset2
+      );
+      return {
+        status: "success",
+        txSig: swapResult.txSig,
+        currentPercentageAsset1,
+        currentPercentageAsset2,
+        swapDirection,
+        amountToSwap: swapResult.amountToSwap,
+        amountToSwapInSOL: swapResult.amountToSwapInSOL,
+        totalAmountInUSD,
+        nav: totalAmountInUSD * (depositorShares / 100),
+      };
+    }
+  } catch (error) {
+    console.error("Error rebalancing with drift", error);
     return {
-      //if no need to rebalance, return success
-      status: "success",
+      status: "error",
       txSig: null,
-      currentPercentageAsset1,
-      currentPercentageAsset2,
+      currentPercentageAsset1: 0,
+      currentPercentageAsset2: 0,
       swapDirection: null,
       amountToSwap: 0,
       amountToSwapInSOL: 0,
-      totalAmountInUSD,
-      nav: totalAmountInUSD * (depositorShares / 100),
-    };
-  }
-
-  const swapDirection =
-    currentPercentageAsset1 > percentageAsset1
-      ? "asset1ToAsset2"
-      : "asset2ToAsset1";
-
-  console.log("swapDirection", swapDirection);
-
-  const expectedAmountAsset1 = (percentageAsset1 / 100) * totalAmountInUSD;
-  const expectedAmountAsset2 = (percentageAsset2 / 100) * totalAmountInUSD;
-
-  console.log("expectedAmountAsset1", expectedAmountAsset1);
-  console.log("expectedAmountAsset2", expectedAmountAsset2);
-
-  if (swapDirection === "asset1ToAsset2") {
-    const swapResult = await swapSolForUsdc(
-      driftClient,
-      formattedTokenAmountAsset1,
-      formattedOraclePriceAsset1,
-      expectedAmountAsset1
-    );
-    return {
-      status: "success",
-      txSig: swapResult.txSig,
-      currentPercentageAsset1,
-      currentPercentageAsset2,
-      swapDirection,
-      amountToSwap: swapResult.amountToSwap,
-      amountToSwapInSOL: swapResult.amountToSwapInSOL,
-      totalAmountInUSD,
-      nav: totalAmountInUSD * (depositorShares / 100),
-    };
-  } else {
-    const swapResult = await swapUsdcForSol(
-      driftClient,
-      formattedOraclePriceAsset1,
-      formattedTokenAmountAsset2,
-      formattedOraclePriceAsset2,
-      expectedAmountAsset2
-    );
-    return {
-      status: "success",
-      txSig: swapResult.txSig,
-      currentPercentageAsset1,
-      currentPercentageAsset2,
-      swapDirection,
-      amountToSwap: swapResult.amountToSwap,
-      amountToSwapInSOL: swapResult.amountToSwapInSOL,
-      totalAmountInUSD,
-      nav: totalAmountInUSD * (depositorShares / 100),
+      totalAmountInUSD: 0,
+      nav: 0,
     };
   }
 };
