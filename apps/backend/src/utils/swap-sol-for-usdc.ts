77efeba999021720552e09c81c6480b7095b18b1
chixx <lucottejeremie@gmail.com>
2025-06-13 15:32:15 +0200

    backend update swap sol for usdc with error handling

--- apps/backend/src/utils/swap-sol-for-usdc.ts
+++ apps/backend/src/utils/swap-sol-for-usdc.ts
@@ -7,25 +7,46 @@ export const swapSolForUsdc = async (
   formattedOraclePriceAsset1: number,
   expectedAmountAsset1: number
 ) => {
-  const amountExpectedAsset1 = Number(
-    expectedAmountAsset1 / Number(formattedOraclePriceAsset1)
-  );
+  try {
+    console.log("----- STARTING SWAP SOL FOR USDC -----");
+    const amountExpectedAsset1 = Number(
+      expectedAmountAsset1 / Number(formattedOraclePriceAsset1)
+    );
 
-  console.log("current amount asset1", formattedTokenAmountAsset1);
-  console.log("amount expected asset1", Number(amountExpectedAsset1));
+    console.log("current amount asset1", formattedTokenAmountAsset1);
+    console.log("amount expected asset1", Number(amountExpectedAsset1));
 
-  const amountToSwap =
-    Number(formattedTokenAmountAsset1) - amountExpectedAsset1;
+    const amountToSwap =
+      Number(formattedTokenAmountAsset1) - amountExpectedAsset1;
 
-  console.log("amount to swap", amountToSwap);
-  const txSig = await sellSolForUsdc(driftClient, {
-    amountSol: amountToSwap < 0 ? formattedTokenAmountAsset1 : amountToSwap,
-  });
-  console.log("Transaction signature:", txSig);
-  return {
-    status: "success",
-    txSig,
-    amountToSwap,
-    amountToSwapInSOL: amountToSwap,
-  };
+    console.log("amount to swap", amountToSwap);
+    try {
+      const txSig = await sellSolForUsdc(driftClient, {
+        amountSol: amountToSwap < 0 ? formattedTokenAmountAsset1 : amountToSwap,
+      });
+      console.log("Transaction signature:", txSig);
+      return {
+        status: "success",
+        txSig,
+        amountToSwap,
+        amountToSwapInSOL: amountToSwap,
+      };
+    } catch (error) {
+      console.error("Error swapping SOL for USDC", error);
+      return {
+        status: "error",
+        txSig: null,
+        amountToSwap: amountToSwap,
+        amountToSwapInSOL: amountToSwap,
+      };
+    }
+  } catch (error) {
+    console.error("Error swapping SOL for USDC", error);
+    return {
+      status: "error",
+      txSig: null,
+      amountToSwap: 0,
+      amountToSwapInSOL: 0,
+    };
+  }
 };
