94f75541e0a1949a841301690f4962c60d3c3906
chixx <lucottejeremie@gmail.com>
2025-06-13 19:37:05 +0200

    backend update limit order with isLimitOrderSet & modifyLimitOrder

--- apps/backend/src/service/limit-order.service.ts
+++ apps/backend/src/service/limit-order.service.ts
@@ -1,32 +1,35 @@
 import { initDrift } from "@src/utils/init-drift";
-import { executeLimitOrder } from "./drift.service";
+import {
+  executeLimitOrder,
+  modifyLimitOrder as modifyLimitOrderDrift,
+} from "./drift.service";
 import { getAssetData } from "@src/utils/get-asset-data";
-import { BN } from "@coral-xyz/anchor";
+import { User } from "@drift-labs/sdk";
+
+const getFormattedTokenAmountRiskyAsset = async (
+  user: User,
+  riskyAsset: string
+) => {
+  const riskyAssetData = getAssetData(riskyAsset);
+  const tokenAmountRiskyAsset = user.getTokenAmount(riskyAssetData.marketIndex);
+
+  return Number(tokenAmountRiskyAsset) / 10 ** riskyAssetData.decimals;
+};
 
 export async function placeLimitOrder(
   riskyAsset: string,
   executionPrice: number
 ) {
   const { driftClient, user } = await initDrift();
-
-  // Get the price of the asset1 and asset2
   const riskyAssetData = getAssetData(riskyAsset);
 
-  // Fetch portfolio Assets balance
-  const tokenAmountRiskyAsset = user.getTokenAmount(riskyAssetData.marketIndex);
-
-  console.log("tokenAmountRiskyAsset", tokenAmountRiskyAsset.toString());
-
   const formattedTokenAmountRiskyAsset =
-    Number(tokenAmountRiskyAsset) / 10 ** riskyAssetData.decimals;
-
-  console.log("formattedTokenAmountRiskyAsset", formattedTokenAmountRiskyAsset);
+    await getFormattedTokenAmountRiskyAsset(user, riskyAsset);
 
   let txSig;
   try {
     txSig = await executeLimitOrder(driftClient, {
-      // amountSol: formattedTokenAmountRiskyAsset,
-      amountSol: 0.1,
+      amountSol: formattedTokenAmountRiskyAsset,
       marketIndex: riskyAssetData.marketIndex,
       executionPrice,
     });
@@ -39,4 +42,66 @@ export async function placeLimitOrder(
   const orders = user.getOpenOrders();
 
   console.log("orders", orders);
+}
+
+export async function modifyLimitOrder(
+  orderId: number,
+  riskyAsset: string,
+  executionPrice: number
+) {
+  try {
+    const { driftClient, user } = await initDrift();
+
+    const order = user.getOrder(orderId);
+
+    const formattedExecutionPrice =
+      driftClient.convertToPricePrecision(executionPrice);
+
+    const formattedTokenAmountRiskyAsset =
+      await getFormattedTokenAmountRiskyAsset(user, riskyAsset);
+
+    const formattedAssetAmount = driftClient.convertToPerpPrecision(
+      formattedTokenAmountRiskyAsset
+    );
+
+    const isTriggerPriceDifferent =
+      order?.triggerPrice?.toString() !== formattedExecutionPrice.toString();
+
+    const isAssetAmountDifferent =
+      order?.baseAssetAmount?.toString() !== formattedAssetAmount.toString();
+
+    console.log("isTriggerPriceDifferent", isTriggerPriceDifferent);
+    console.log("isAssetAmountDifferent", isAssetAmountDifferent);
+
+    if (isTriggerPriceDifferent || isAssetAmountDifferent) {
+      const txSig = await modifyLimitOrderDrift(
+        driftClient,
+        orderId,
+        formattedTokenAmountRiskyAsset,
+        executionPrice
+      );
+
+      return txSig;
+    } else {
+      console.log(
+        "Order price and asset amount is already set to the desired price"
+      );
+      return null;
+    }
+  } catch (error) {
+    console.log("error", error);
+    return null;
+  }
+}
+
+export async function isLimitOrderSet() {
+  const { user } = await initDrift();
+
+  const orders = user.getOpenOrders();
+
+  if (orders.length > 0) {
+    return { isSet: true, orderId: orders[0].orderId };
+  } else {
+    return { isSet: false, orderId: 0 };
+  }
 }
