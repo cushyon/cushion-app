18709fe304e0e2289554b822070db288370e94f9
nada-r <nadarzer@gmail.com>
2025-06-18 14:11:58 +0200

    adjust powered by section

--- apps/landing/components/powered-by.tsx
+++ apps/landing/components/powered-by.tsx
@@ -4,7 +4,9 @@ export default function PoweredBy() {
   return (
     <div className="mt-32 pt-16">
       <div className="text-center">
-        <h3 className="text-gray-500 text-3xl mb-8">Powered by</h3>
+        <h3 className="text-[#64748B] text-center text-[50px] leading-[80px] mb-8">
+          Powered by
+        </h3>
         <div className="flex items-center justify-center gap-12 md:gap-16">
           <Image src="/jup_x2.png" alt="Jupiter" width={176} height={59} />
           <Image src="/drift_png.png" alt="Drift" width={176} height={59} />
